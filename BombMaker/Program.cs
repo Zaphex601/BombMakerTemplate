9ab89ac1a4ea23abec6abc3592773664fe21d42d
Zaphex601 <yesar7464@gmail.com>
2021-07-25 15:13:26 +0800

    Add project files.

---
+++ BombMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BombMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
